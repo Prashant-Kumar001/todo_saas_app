f96695f32e196c03711d1b7ad849e2f9a07416d7
Prashant Kumar <prashant0kumar101@gmail.com>
2026-03-06 11:22:56 +0530

    again

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,15 +1,12 @@
 import { NextResponse } from "next/server"
 import { auth } from "@clerk/nextjs/server";
 import { prisma } from "@/lib/prisma";
-import { normalizeSubscription } from "@/lib/helper";
 
 export async function GET() {
     try {
         const { userId } = await auth();
         if (!userId)
             return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-
-        await normalizeSubscription(userId);
 
         const todos = await prisma.todo.findMany({
             where: { userId },
